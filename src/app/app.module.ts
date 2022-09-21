6e434e7f3193c79fba17e47c28a29baf63a707ad
Nathan Mangrum <nathan.mangrum@jbhunt.com>
2022-09-21 09:47:42 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { GroupsComponent } from './groups/groups.component';
+import { WhiskeyTypesComponent } from './whiskey-types/whiskey-types.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    GroupsComponent,
+    WhiskeyTypesComponent
   ],
   imports: [
     BrowserModule,
